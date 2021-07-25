3c8ac530e08417c916ae42b78ece8c3bb701f267
Jack Ryan <jack.dixon.ryan@gmail.com>
2021-07-25 12:10:54 -0600

    Whittled away CRUD operations to roll back to the basic fetch.

--- src/routes/characters/data.json.ts
+++ src/routes/characters/data.json.ts
@@ -7,37 +7,3 @@ export async function get({ params }) {
     }
   }
 }
-
-export async function post(request) {
-  const { body } = request;
-  const { character } = body;
-  if (!character) {
-    return {
-      status: 400,
-      body: {
-        error: "Request must include the `character` property."
-      }
-    }
-  }
-
-  const { name, level, created } = character;
-  if (!name || !level || !created) {
-    return {
-      status: 400,
-      body: {
-        error: "Character was missing required fields."
-      }
-    }
-  }
-
-  // for our shitty ID system.
-  character.id = (characters.length + 1).toString();
-
-  characters.push(character);
-  return {
-    status: 201,
-    body: {
-      characters
-    }
-  }
-}
