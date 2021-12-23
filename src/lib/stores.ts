95401579b1fd4b197ce9ae2de275769c16575674
jackdixonryan <jack.dixon.ryan@gmail.com>
2021-12-23 08:47:23 -0700

    Update to the store - bug with weird loading owed to lack of listening for auth state change event.

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,7 +1,12 @@
 import { writable } from "svelte/store";
 import supabase from "$lib/supabase";
+import type { AuthChangeEvent, Session } from "@supabase/supabase-js";
 const { auth } = supabase;
 
 // EXPORTING STORES 
 
 export const user = writable(auth.user());
+
+auth.onAuthStateChange((event: AuthChangeEvent, session: Session) => {
+  user.set(auth.user());
+});
