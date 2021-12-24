58c055aaf421568743ab57e76ebd9ad652532ed2
jackdixonryan <jack.dixon.ryan@gmail.com>
2021-12-24 16:35:26 -0700

    Working on character creator off and on.

---
+++ src/components/CharacterCreator/CharacterCreatorSpecies.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  const speciesList = [
+    { 
+      name: "human",
+      description: "Middle height, rounded ears, hair atop the head. Otherwise, oddly hairless."
+    },
+    { 
+      name: "elf",
+      description: "Taller than ordinary, pointed ears, otherwise basically a human. Oh, immortal."
+    }, 
+    { 
+      name: "dwarf",
+      description: "Short, stout, hairy."
+    }, 
+    {
+      name: "goblin",
+      description: "short, skinny, typically green."
+    }, 
+    { 
+      name: "orc",
+      description: "Goblin, only taller and muscular."
+    }, 
+    {
+      name: "satyr",
+      description: "The top bit is a gnome. The bottom bit is a goat."
+    }, 
+    { 
+      name: "gnome",
+      description: "Short, hairy, but decidedly not stout."
+    }, 
+    { 
+      name: "hobbit",
+      description: "Short, stout, but the hair tends to be on the feet."
+    }
+  ]
+</script>
+
+<h1>Species</h1>
+<p>Although SQ will encourage people to build their own species for more immersive, intricate campaigns, we will offer a couple starter species for those players who are looking for the more traditional fantasy route.</p>
+<h3>Species List </h3>
+<ul>
+  { #each speciesList as species }
+  <article>
+    <h4>{ species.name.toUpperCase() }</h4>
+    <p>{ species.description }</p>
+    <footer>
+      <button class="secondary outline" on:click={() => dispatch('species-complete', species.name )}>
+        Select
+      </button>
+    </footer>
+  </article>
+  { /each }
+</ul>
--- src/routes/characters/create.svelte
+++ src/routes/characters/create.svelte
@@ -1,41 +1,81 @@
 <script lang="ts">
   import CharacterCreatorIntroduction from "../../components/CharacterCreator/CharacterCreatorIntroduction.svelte";
+  import CharacterCreatorBio from "../../components/CharacterCreator/CharacterCreatorBio.svelte";
+  import CharacterCreatorSpecies from "../../components/CharacterCreator/CharacterCreatorSpecies.svelte";
+
+  class CharacterCreator {
+    character: any;
+    complete: boolean;
+    completeByStep: any;
+    currentStep: string;
+    constructor() {
+      this.character = {
+        // start with some basic string fields to show how it works. 
+        name: null,
+        age: null,
+        appearance: null,
+        bio: null,
+        species: null,
+      },
+      this.complete = false;
+      this.completeByStep = {
+        bio: false,
+        species: false,
+        skills: false,
+        inventory: false,
+      }
+      this.currentStep = "species";
+    }  
+  }
+
+  const creator = new CharacterCreator();
 
   function handleStart() {
-    console.log("Looks like we started!");
+    creator.currentStep = "bio";
+  }
+
+  function handleBioComplete(event) {
+    const bio = event.detail;
+    const { characterAppearance, characterBio, characterName } = bio;
+
+    creator.character.name = characterName;
+    creator.character.appearance = characterAppearance;
+    creator.character.bio = characterBio;
+    if (characterName) {
+      creator.currentStep = "species";
+    }// error is handled elsewhere. 
+  }
+
+  function handleSpeciesComplete(event) {
+    const species = event.detail;
+    creator.character.species = species;
   }
 </script>
 
-<style>
-  .bottom-nav {
-    position: fixed;
-    bottom: 0;
-    padding-left: 2rem;
-    padding-right: 2rem;
-    left: 0;
-    right: 0;
-    border-top: 1px gray solid;
-  }
-</style>
-
 <div>
+  { #if creator.currentStep === "start" }
   <CharacterCreatorIntroduction on:start={handleStart}></CharacterCreatorIntroduction>
+  { :else if creator.currentStep === "bio" }
+  <CharacterCreatorBio on:bio-complete={handleBioComplete}></CharacterCreatorBio>
+  { :else if creator.currentStep === "species"}
+  <CharacterCreatorSpecies on:species-complete={handleSpeciesComplete}></CharacterCreatorSpecies>
+  { /if }
   <nav class="bottom-nav">
     <ul>
       <li><strong>Character Creator</strong></li>
     </ul>
     <ul>
       <li>
-        <button class="outline">Bio</button>
+        <button class="outline" on:click={() => creator.currentStep = "bio"}>Bio</button>
       </li>
       <li>
-        <button class="outline">Species</button>
+        <button class="outline" on:click={() => creator.currentStep = "species"}>Species</button>
       </li>
       <li>
-        <button class="outline">Skills</button>
+        <button class="outline" on:click={() => creator.currentStep = "skills"}>Skills</button>
       </li>
       <li>
-        <button class="outline">Inventory</button>
+        <button class="outline" on:click={() => creator.currentStep = "inventory"}>Inventory</button>
       </li>
     </ul>
   </nav>
